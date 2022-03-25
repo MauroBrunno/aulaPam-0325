ab557af99bc6a8fc06d78fa96d6c8cea54341dea
unknown <mbrunno@outlook.com>
2022-03-25 16:37:43 -0300

    aulaSobreComponentes

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Cabecalho from './src/components/cabecalho';
+
+export default function App() {
+  return (
+    <View>
+      <Cabecalho/>
+    </View>
+  );
+}
