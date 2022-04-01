0704c3b6291bd203f9a68733e1c3cf9f238a8d18
unknown <mbrunno@outlook.com>
2022-04-01 16:30:43 -0300

    adicao de botoes

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Cabecalho from './src/components/cabecalho';
+import Botao from './src/components/botao';
 
 export default function App() {
   return (
     <View>
       <Cabecalho/>
+      <Botao
+        logo="md-game-controller-sharp"
+        texto="Jogos"
+        cor='darkorange'
+      />
+      
     </View>
   );
 }
