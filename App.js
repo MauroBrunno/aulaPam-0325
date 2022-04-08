8be6ab5a9015265a13758e1c0b4ba167b10b6191
unknown <mbrunno@outlook.com>
2022-04-08 15:25:30 -0300

    adicionando botões

--- App.js
+++ App.js
@@ -8,11 +8,32 @@ export default function App() {
     <View>
       <Cabecalho/>
       <Botao
-        logo="md-game-controller-sharp"
+        Logo="md-game-controller-sharp"
         texto="Jogos"
         cor='darkorange'
+
+        Logo2="book"
+        texto2="Livros"
+        cor2="red"
       />
-      
+      <Botao
+        Logo="calculator-sharp"
+        texto="Aplicativos"
+        cor='blue'
+
+        Logo2="play"
+        texto2="Filmes"
+        cor2="red"
+      />
+        <Botao
+        Logo="bar-chart"
+        texto="Em Alta"
+        cor='gray'
+
+        Logo2="apps"
+        texto2="Categorias"
+        cor2="red"
+      />
     </View>
   );
 }
